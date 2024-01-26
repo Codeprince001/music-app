df8c31fbbce1a3e73818dd9af713c9a1252c7c5c
Codeprince001 <urangyorkwisdom@gmail.com>
2024-01-26 21:26:18 +0100

    added new image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import loader from './loader.svg';
 import logo from './logo.svg';
+import error from "./error.gif";
+import music from "./listen-music.jpg";
 
 export {
   logo,
   loader,
+  error,
+  music
 };
