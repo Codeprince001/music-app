fca0b88342267ad1985ea8241f3cb7cd8d644fbb
Codeprince001 <urangyorkwisdom@gmail.com>
2024-01-31 08:11:12 +0100

    fix: discover component:

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,17 +7,18 @@ const Discover = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const genreTitle = "pop";
+
   const { data, isFetching, error, isSuccess } = useGetTopChartsQuery();
 
   if (isFetching) return <Loader title="Loading Songs..." />;
   if (error) return <Error />;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col place-content-center ">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white">Discover</h2>
         <select
-          key={genreTitle}
+          key={{}}
           onChange={() => { }}
           value=""
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
@@ -28,7 +29,7 @@ const Discover = () => {
         </select>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start">
+      <div className="flex flex-wrap items-center sm:justify-start">
         {
           data?.tracks?.map((song, i) =>
             < SongCard key={song.key} song={song} i={{ i }} isPlaying={isPlaying} activeSong={activeSong} data={data} />
