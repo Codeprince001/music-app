9c9edb00c291bfa06699f2a8e33944aecb088bf7
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-06 14:16:33 +0100

    export constants

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import loader from './loader.svg';
 import logo from './logo.svg';
 import error from "./error.gif";
 import music from "./listen-music.jpg";
+import noImg from "./no-song-img.jpg";
 
 export {
   logo,
   loader,
   error,
-  music
+  music,
+  noImg
 };
