b2c6e08226a4c7dd3b24dbb5875e6dfac2a6f827
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-07 16:15:18 +0100

    feat: added lyrics functionality to song Details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,24 +1,45 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { ShazamCoreApi, useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { useEffect, useState } from "react";
+import { unwrapResult } from "@reduxjs/toolkit";
 
 const SongDetails = () => {
-  const { songid } = useParams();
-  console.log(Number(songid));
-  const { data: songDetailData, isFetching } = useGetSongDetailsQuery(Number(songid));
   const dispatch = useDispatch();
+  const { songid } = useParams();
+  const [songDetailData, setSongDetailData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [lyricsId, setLyricsId] = useState();
+  const [shazamSongId, setShazamSongId] = useState();
   const { setActiveSong, isPlaying } = useSelector((state) => state.player);
-  console.log(songDetailData);
-  const lyricsId = songDetailData?.resources?.[`shazam-songs`]?.[songid].relationships.lyrics?.data?.[0].id;
-  const { resources } = songDetailData;
 
-  if (isFetching || isLoading) return (<Loader />);
-  if (isSuccess) return (console.log("fetchedsuccessful"));
+
+  useEffect(() => {
+    let Mounted = false;
+    dispatch(ShazamCoreApi.endpoints.getSongDetails.initiate(songid), { forceRefetch: true }).unwrap()
+      .then(result => {
+        setIsLoading(true);
+        setSongDetailData(result);
+        setLyricsId(result?.resources?.[`shazam-songs`]?.[songid].relationships?.lyrics?.data[0].id);
+        setSongDetailData(result?.resources?.[`shazam-songs`]?.[songid].relationships?.songs?.data[0].id);
+      }).catch(error => {
+        console.log(error);
+        setIsLoading(false);
+      });
+
+    return () => {
+      Mounted = false;
+    };
+  }, [dispatch, songid]);
+
+  console.log(shazamSongId);
+
+
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={""} songData={songDetailData} /> */}
+      {/* <DetailsHeader artistId={""} songData={songDetailData} songid={songid} songDetailId={shazamSongId} /> */}
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
@@ -26,7 +47,7 @@ const SongDetails = () => {
 
       <div className="mt-5 w-[500px] m-auto">
         {
-          songDetailData ? <p className="text-white text-1xl font-bold text-center">{resources.lyrics?.[lyricsId]?.attributes?.text}</p> : <p className="text-white text-3xl font-bold text-center">Sorry, No Lyrics</p>
+          songDetailData && <p className="text-white text-1xl font-bold text-center">{songDetailData?.resources?.lyrics?.[lyricsId]?.attributes?.text.join(" \n ")}</p>
         }
       </div>
     </div>
