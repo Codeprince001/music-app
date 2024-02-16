692d767750145a381b3ecf19a601c8ef0d289502
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-16 23:12:18 +0100

    added lyrics detail header component

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,21 +1,25 @@
 import { Link } from "react-router-dom";
-import { useGetSongDetails_2Query, useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 
 
 
-const DetailsHeader = ({ artistId, artistData, songData, songid, shazamSongId }) => {
+const DetailsHeader = ({ shazamSongId, artistArtwork, songData, title }) => {
 
-  const { data, isFetching } = useGetSongDetails_2Query(shazamSongId);
-  console.log(shazamSongId);
+  const { data } = useGetArtistDetailsQuery(shazamSongId);
+  console.log(songData);
   console.log(data);
 
 
   return (
 
     < div className="relative w-full flex flex-col" >
-      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-32 h-28 mb-4" />
       <div className="absolute inset-0 flex items-center">
-        <img alt="art" src={songData} />
+        <img alt="art" src={artistArtwork} width="54px" className="rounded-full -ml-2" />
+
+        <div className="text-cyan-600 font-bold xl:text-3xl text-2xl ml-3">
+          {title}
+        </div>
       </div>
     </div >
   );
