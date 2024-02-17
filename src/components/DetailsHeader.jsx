0d4355dcd0d9298fc368c06036dce3a07fa5ea26
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-17 17:20:16 +0100

    fix: Deetail Header completely set up, feat: added new styling

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,24 +1,20 @@
 import { Link } from "react-router-dom";
-import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 
 
 
-const DetailsHeader = ({ shazamSongId, artistArtwork, songData, title }) => {
-
-  const { data } = useGetArtistDetailsQuery(shazamSongId);
-  console.log(songData);
-  console.log(data);
+const DetailsHeader = ({ shazamSongId, artistArtwork, songData, title, artistData }) => {
 
 
   return (
 
     < div className="relative w-full flex flex-col" >
-      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-32 h-28 mb-4" />
+      <div className="w-full bg-gradient-to-l  to-black sm:h-32 h-28 mb-4 rounded-2xl shadow-lg shadow-indigo-500/50" />
       <div className="absolute inset-0 flex items-center">
-        <img alt="art" src={artistArtwork} width="54px" className="rounded-full -ml-2" />
+        <img alt="art" src={artistData.artistArtwork} width="64px" className="cursor-pointer rounded-full ml-2 shadow-lg shadow-indigo-500/50" />
 
-        <div className="text-cyan-600 font-bold xl:text-3xl text-2xl ml-3">
-          {title}
+        <div className="text-xl ml-3 cursor-pointer">
+          <p className="text-cyan-500 font-bold md:text-3xl ">{artistData.title}</p>
+          <p className="text-gray-300 font-bold text-sm">{artistData.artist}</p>
         </div>
       </div>
     </div >
