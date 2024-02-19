c1b431c0a4651dbc2682cf21903ebdfded185c58
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-20 00:34:14 +0100

    added conditional rendeering for img

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 
 
-const DetailsHeader = ({ shazamSongId, artistArtwork, songData, title, artistData }) => {
+const DetailsHeader = ({ artistArtwork, songData, title, artistData }) => {
 
 
   return (
@@ -10,7 +10,7 @@ const DetailsHeader = ({ shazamSongId, artistArtwork, songData, title, artistDat
     < div className="relative w-full flex flex-col" >
       <div className="w-full bg-gradient-to-l  to-black sm:h-32 h-28 mb-4 rounded-2xl shadow-lg shadow-indigo-500/50" />
       <div className="absolute inset-0 flex items-center">
-        <img alt="art" src={artistData.artistArtwork} width="64px" className="cursor-pointer rounded-full ml-2 shadow-lg shadow-indigo-500/50" />
+        <img alt="art" src={!artistData.artistArtwork?.artistAvatar ? artistData?.artistArtwork?.coverArt : artistData?.artistArtwork?.artistAvatar} width="64px" className="cursor-pointer rounded-full ml-2 shadow-lg shadow-indigo-500/50" />
 
         <div className="text-xl ml-3 cursor-pointer">
           <p className="text-cyan-500 font-bold md:text-3xl ">{artistData.title}</p>
