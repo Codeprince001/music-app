cf44a50628de4e3b371bdb71394b0c12ca6dd031
Codeprince001 <urangyorkwisdom@gmail.com>
2024-02-20 00:27:42 +0100

    component to display similar songs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,33 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from "./SongBar";
+import { useGetSongRelatedQuery } from "../redux/services/shazamCore";
+import Loader from "./Loader";
+
+
+const RelatedSongs = ({ activeSong,
+  handlePauseClick,
+  handlePlayClick, isPlaying, artistData }) => {
+
+
+  console.log(artistData.relatedSongId);
+  const { data, isLoading } = useGetSongRelatedQuery("track-similarities-id-668834911");
+
+
+  if (isLoading) return (<Loader title="Loading related song" />);
+  const relatedSongs = Object.values(data?.resources?.["shazam-songs"]);
+
+  return (
+
+    <div className="flex flex-col mt-6">
+      <h1 className="font-bold text-2xl text-white sticky top-0">Related Songs</h1>
+
+      <div className="mt-6 w-full flex flex-col">
+        {relatedSongs.map((song, i) => {
+          return <SongBar song={song} isPlaying={isPlaying} i={i} activeSong={activeSong} handlePauseClick={handlePauseClick} handlePlayClick={handlePlayClick} artistData={artistData} />;
+        })}
+      </div>
+
+    </div>
+  );
+};
 
 export default RelatedSongs;
