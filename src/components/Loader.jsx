e3c2b6f8063e09a3e299e0142375c168eb0f4eb8
Codeprince001 <urangyorkwisdom@gmail.com>
2024-03-30 10:27:59 +0100

    update loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,9 @@ import { Skeleton, SkeletonCircle, SkeletonText, Box } from '@chakra-ui/react';
 
 
 const Loader = ({ title, isLoading }) => (
-  <Skeleton isLoaded={isLoading} width="50px" height="50px">
+  <Skeleton isLoaded={isLoading}>
     <div className="w-full flex justify-center items-center flex-col">
-      <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
+      <img src={loader} alt="loader" className="w-full h-32 object-contain" />
       <h1 className="font-bold text-2xl text-white mt-2">{title || "loading..."}</h1>
     </div>
   </Skeleton>
